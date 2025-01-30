94744a7b021a5c8ef8d5de5894b25141ce6743f4
Jskebede <161852134+Jskebede@users.noreply.github.com>
2025-01-30 04:20:14 +0100

    color and structure change

--- src/components/HowToUseDialog.tsx
+++ src/components/HowToUseDialog.tsx
@@ -23,19 +23,20 @@ export const HowToUseDialog = () => {
         </DialogHeader>
         <div className="space-y-4">
           <div>
-            <h3 className="font-medium text-pink-700 mb-2">Steg 1: Skapa Stationer</h3>
+            <h3 className="font-medium text-white-700 mb-2">Steg 1: Skapa Stationer</h3>
             <p className="text-sm text-muted-foreground">
-              Börja alltid med att skapa dina stationer. Använd "Lägg till Station" knappen för att lägga till varje arbetsstation
+              Börja alltid med att skapa dina stationer. Använd "Station" knappen för att lägga till arbetsstationer
               och ange hur många Grisar som behövs för varje station. 
-              </p>
+              </p>  
               <hr/>
+              <br />
               <p className="text-sm text-muted-foreground">
               (!!!VIKTIGT!!! Pga min konstiga algorithm så lägg till "Plock" stationen sist.)
               (Detta är ända momentet som det behövs att använda tankekraft, då du behöver veta hur många det är som jobbar och hur många av dem grisarna man vill använda på varje station.)
             </p>
           </div>
           <div>
-            <h3 className="font-medium text-pink-700 mb-2">Steg 2: Lägg till Grisar</h3>
+            <h3 className="font-medium text-white-700 mb-2">Steg 2: Lägg till Grisar</h3>
             <p className="text-sm text-muted-foreground">
               Lägg till Grisar enligt dina totala stationskrav. Till exempel:
             </p>
@@ -45,7 +46,7 @@ export const HowToUseDialog = () => {
             </ul>
           </div>
           <div>
-            <h3 className="font-medium text-pink-700 mb-2">Steg 3: Generera Schema</h3>
+            <h3 className="font-medium text-white-700 mb-2">Steg 3: Generera Schema</h3>
             <p className="text-sm text-muted-foreground">
               När du har lagt till alla stationer och Grisar, klicka på "Generera Schema" knappen
               för att skapa ditt schema. Systemet kommer automatiskt att tilldela Grisar till stationer baserat på
@@ -53,7 +54,7 @@ export const HowToUseDialog = () => {
             </p>
           </div>
           <div>
-            <h3 className="font-medium text-pink-700 mb-2">OBS: Avstå helst från att använda flygande </h3>
+            <h3 className="font-medium text-white-700 mb-2">OBS: Avstå helst från att använda flygande </h3>
             <p className="text-sm text-muted-foreground">
                 Vill du så gärna lägga till dig själv i Schemat som flygande så gör du såhär: 
                 1. Lägg till alla stationer FÖRUTOM flygande. 
